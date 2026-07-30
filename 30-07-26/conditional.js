759c93ae9c56632b22a5feff22caaeaae3834eee
VamsiKrishna-11 <vamsikrishna.malneedy@gmail.com>
2026-07-30 15:15:21 +0530

    java script condiotional statements

---
+++ 30-07-26/conditional.js
@@ -0,0 +1,13 @@
+let a = 84;
+let b = 45;
+let c = 86;
+
+if (a>b && a>c){
+    document.write("a is the largest number")
+}else if(b>a && b>c){
+    document.write("b is the largest number")
+}else if (c>a && c>b){
+    document.write("c is the largest number")
+}else{
+    document.write("all numbers are equal")
+}
